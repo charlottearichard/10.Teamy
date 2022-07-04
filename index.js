7886adbc35663d941ce7f75a391b1a5b1d15aaa4
Charlotte <charlottealiciarichard@gmail.com>
2022-07-04 18:18:55 -0400

    Inquirer Manager

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
 // NPM
-const inquire = require("inquirer");
 const generateTeamy = require("./src/teamytemp.js");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
 
-const generateTeam = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is team members name?",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Enter name");
-          return false;
-        }
-      },
+const teamQuestions = [
+  //Managers Questions
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team managers name?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Don't forget to enter team managers name!");
+        return false;
+      }
     },
-  ]);
-};
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide the team managers email: ",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Don't forget to enter team managers Id!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please provide team managers id:",
+    validate: (idInput) => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Don't forget to enter the Id!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "Please provide team managers office number:",
+    validate: (officeInput) => {
+      if (officeInput) {
+        return true;
+      } else {
+        console.log("Don't forget to enter the office number!");
+        return false;
+      }
+    },
+  },
+];
+
+function init() {
+  inquirer.prompt(teamQuestions);
+}
+init();
